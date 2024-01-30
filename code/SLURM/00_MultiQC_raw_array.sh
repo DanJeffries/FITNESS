38bc367366b06df642c25135713eca015309ab3a
DanJeffries <dj20y461@submit01.ubelix.unibe.ch>
2024-01-30 11:55:44 +0100

    Jan24 update

--- code/SLURM/00_MultiQC_raw_array.sh
+++ code/SLURM/00_MultiQC_raw_array.sh
@@ -15,7 +15,7 @@
 module load UHTS/Analysis/MultiQC/1.8
 
 RUN_DATA_DIR=/storage/research/iee_evol/DanJ/Stickleback/G_aculeatus/FITNESS/raw_BACKUP/FITNESS_Run${SLURM_ARRAY_TASK_ID}
-RUN_OUT_DIR=/storage/scratch/iee/dj20y461/Stickleback/G_aculeatus/FITNESS/data/MultiQC/FITNESS_Run${SLURM_ARRAY_TASK_ID}
+RUN_OUT_DIR=/storage/scratch/iee/dj20y461/Stickleback/G_aculeatus/FITNESS/data/MultiQC_raw/FITNESS_Run${SLURM_ARRAY_TASK_ID}
 
 if [ ! -d "$RUN_OUT_DIR" ]
 then
